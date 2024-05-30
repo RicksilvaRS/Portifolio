e3ac3d5c459b3980cac976d1a1c26b5e9b617d47
RicksilvaRS <rickmycf@gmail.com>
2024-05-30 16:36:22 -0300

    adicionando o menu-mobile para acessar as opções de menu

---
+++ script.js
@@ -0,0 +1,15 @@
+let rickMenu = document.getElementById("rick-menu");
+let menu = document.getElementById("menu-mobile");
+let overlay = document.getElementById("overlay-menu");
+
+rickMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});

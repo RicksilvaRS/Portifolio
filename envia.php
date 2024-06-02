0b11c1a6ba6269336fa8d5890c00315733c70c35
RicksilvaRS <rickmycf@gmail.com>
2024-06-01 22:05:17 -0300

    corrigindo pela segunda vez o erro do php

--- envia.php
+++ envia.php
@@ -3,15 +3,15 @@
 $nome = addslashes($_POST['nome']);
 $email = addcslashes($_POST['email']);
 $celular = addcslashes($_POST['celular']);
-
+$mensagem = addcslashes($_POST['mensagem']);
 $para = "rickmygiliard@gmail.com";
 $assunto = = "coleta de dados - Portifólio";
 
-$corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$celular;
+$corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$celular."\n"."Mensagem: ".$mensagem;
 
 $cabeca = "From: rickmycf@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
-if(mail($para, $assunto, $corpo, $cabeca)){
+if(mail($para,$assunto,$corpo,$cabeca)){
     echo("email enviado com o sucesso");
 }else{
     echo("houve um erro");
